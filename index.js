68caf790b35263e176f9ab8c789a2713d0b7e99b
Dinesh <cat505600.com>
2024-07-19 05:17:51 +0530

    added cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors= require('cors')
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 // Initialize express app
 const app = express();
+app.use(cors())
 app.use(bodyParser.json());
 
 // MongoDB connection
